9a9499abe3a6e80865880dd6a02bfd6d10651131
javiergranizoaguilar <javiercelia123@gmail.com>
2025-11-10 14:24:03 +0100

    new file:   2TDAW_DIW_Practica_2_Web_Cliente.pdf 	modified:   Desarrollo web en entorno cliente/Entregas/Tema1-2/Entrega2/Minas.js 	modified:   Desarrollo web en entorno cliente/Entregas/Tema1-2/Entrega2/home.html 	new file:   "dise\303\261o/Proyecto 3/html/home.html" 	new file:   "dise\303\261o/Proyecto 3/stiles/home.css"

--- Desarrollo web en entorno cliente/Entregas/Tema1-2/Entrega2/Minas.js
+++ Desarrollo web en entorno cliente/Entregas/Tema1-2/Entrega2/Minas.js
@@ -1,10 +1,10 @@
 'use strict'
-function BuscaMinas() {
-    let size = pedirNumero(1, "Dame el tamaño de del tablero");
+function buscaMinas() {
+    let size = pedirNumero(1, "Dame el tamaño de del tablero", "Dame un numero mallor o igual a: ");
     let minesratio = pedirNumeroRango(0, 100);
-    let matrix = TableroCreation(size, minesratio);
-    let matrixShow = TableroCreation(size, 0);
-    matrix = vacioslogic(matrix);
+    let matrix = tableroCreation(size, minesratio);
+    let matrixShow = tableroCreation(size, 0);
+    matrix = vaciosLogic(matrix);
     console.table(matrix);
     console.table(matrixShow);
     play(matrix, matrixShow);
@@ -15,7 +15,6 @@ function play(matrix, matrixShow) {
         if (seleccionResult(matrix, matrixShow) === null || ganar(matrix, matrixShow)) {
             verdad = false
         }
-        pintar(matrixShow);
         console.table(matrixShow);
     } while (verdad);
     if (ganar(matrix, matrixShow)) {
@@ -40,13 +39,13 @@ function ganar(matrix, matrixShow) {
     return win;
 }
 function seleccionResult(matrix, matrixShow) {
-    let CoordX = pedirNumero(-1, "Dame la coordenad X");
-    let CoordY = pedirNumero(-1, "Dame la coordenad Y");
+    let coordX = pedirNumero(-1, "Dame la coordenad X", "Dame un numero mallor o igual a: ");
+    let coordY = pedirNumero(-1, "Dame la coordenad Y", "Dame un numero mallor o igual a: ");
 
     if (matrix[CoordX][CoordY] == "*") {
         return null;
     } else {
-        return show(matrix, matrixShow, CoordX, CoordY);
+        return show(matrix, matrixShow, coordX, coordY);
     }
 }
 function show(matrix, matrixShow, rx, ry) {
@@ -62,44 +61,21 @@ function show(matrix, matrixShow, rx, ry) {
     }
     return aux;
 }
-function vacioslogic(rejaI) {
-
+function vaciosLogic(rejaI) {
+    let minas = 0;
     let aux1 = [];
     let aux2 = [];
     let x1 = 0;
     let x2 = 0;
-    let y1 = 0;
     let y2 = 0;
-    let minas = 0;
+    let aux;
     for (let x = 0; x < rejaI.length; x++) {
         aux2 = [];
         for (let y = 0; y < rejaI.length; y++) {
-
-            switch (true) {
-                case x - 1 < 0:
-                    x1 = 0
-                    x2 = x + 1
-                    break;
-                case x + 1 >= rejaI.length:
-                    x1 = x - 1
-                    x2 = rejaI.length - 1;
-                    break;
-                default:
-                    x1 = x - 1
-                    x2 = x + 1
-                    break;
-            }
-            switch (true) {
-                case y - 1 < 0:
-                    y2 = y + 1
-                    break;
-                case y + 1 >= rejaI.length:
-                    y2 = rejaI.length - 1;
-                    break;
-                default:
-                    y2 = y + 1
-                    break;
-            }
+            aux = borderMargin(x, y, x1, x2, y2, rejaI)
+            x1 = aux.newX1;
+            x2 = aux.newX2;
+            y2 = aux.newY2;
 
             minas = 0;
             for (x1; x1 <= x2; x1++) {
@@ -112,36 +88,29 @@ function vacioslogic(rejaI) {
                     }
                 }
             }
-            switch (true) {
-                case minas != 0:
-                    aux2.push(minas);
-                    break;
-                case rejaI[x][y] === "*":
-                    aux2.push("*");
-                    break;
-                case minas === 0:
-                    aux2.push(".");
-                    break;
-
-            }
+            placingMines(minas, rejaI, aux2)
         }
         aux1.push(aux2);
     }
     return aux1;
 }
-function TableroCreation(size, ratio) {
+function placingMines(mines, rejaI, aux2, x, y) {
+    if (rejaI[x][y] === "*") {
+        aux2.push("*")
+    } else if (mines === 0) {
+        aux2.push(".")
+    } else {
+        aux2.push(mines)
+    }
+}
+function tableroCreation(size, ratio) {
     let matrix = [];
     let aux = []
     for (let x = 0; x < size; x++) {
         aux = [];
         for (let y = 0; y < size; y++) {
-            if (ratio !== 0) {
-                if (Math.floor(Math.random() * 100) <= ratio) {
-                    aux.push("*");
-                }
-                else {
-                    aux.push("X");
-                }
+            if (ratio !== 0 && Math.floor(Math.random() * 100) <= ratio) {
+                aux.push("*");
             }
             else {
                 aux.push("X");
@@ -151,7 +120,7 @@ function TableroCreation(size, ratio) {
     }
     return matrix;
 }
-function pedirNumero(num, text) {
+function pedirNumeroEntero(num, text, textError) {
     let numero;
     let entrada;
 
@@ -160,11 +129,11 @@ function pedirNumero(num, text) {
 
         // Si el usuario cancela o deja vacío, salimos retornando null
         if (entrada === null || entrada.trim() === "") {
-            prompt("Dame un numero mallor a 1");
+            prompt(textError);
         }
 
         // Convertimos a número y validamos
-        numero = parseFloat(entrada);
+        numero = parseInt(entrada + num);
 
     } while (isNaN(numero) || numero <= num);
 
@@ -178,51 +147,54 @@ function pedirNumeroRango(num1, num2) {
         // Convertimos a número y validamos
         numero = parseFloat(numero);
 
-        // Verificamos si:
-        // - No es un número válido (isNaN)
-        if (isNaN(numero)) {
-
-            numero = null; // Forzamos a que siga en el bucle
-        }
     } while ((numero === null || isNaN(numero)) && !(numero <= num1 && numero >= num2));
 
     return numero;
+}
+function borderMargin(x, y, x1, x2, y2, rejaI) {
+
+    if (x + -1 < 0) {
+        x1 = 0;
+        x2 = x + 1;
+    }
+    if (x + 1 >= rejaI.length) {
+        x1 = x - 1;
+        x2 = rejaI.length - 1;
+    }
+    if (!(x + 1 >= rejaI.length && x + -1 < 0)) {
+        x1 = x - 1;
+        x2 = x + 1;
+    }
+    if (y + -1 < 0) {
+        y2 = y + 1
+    }
+    if (y + 1 >= rejaI.length) {
+        y2 = rejaI.length - 1;
+    }
+    if (!(y + 1 >= rejaI.length && y + -1 < 0)) {
+        y2 = y + 1
+    }
+    return {
+        newX1: x1,
+        newX2: x2,
+        newY2: y2
+    }
+
 }
 function desbloqueoblancos(rejaI, matrixShow, rx, ry) {
     let x1 = 0;
     let x2 = 0;
     let y1 = 0;
     let y2 = 0;
-    let minas = 0;
+    let aux;
     for (let x = 0; x < rejaI.length; x++) {
         for (let y = 0; y < rejaI.length; y++) {
-            switch (true) {
-                case x - 1 < 0:
-                    x1 = 0
-                    x2 = x + 1
-                    break;
-                case x + 1 >= rejaI.length:
-                    x1 = x - 1
-                    x2 = rejaI.length - 1;
-                    break;
-                default:
-                    x1 = x - 1
-                    x2 = x + 1
-                    break;
-            }
-            switch (true) {
-                case y - 1 < 0:
-                    y2 = y + 1
-                    break;
-                case y + 1 >= rejaI.length:
-                    y2 = rejaI.length - 1;
-                    break;
-                default:
-                    y2 = y + 1
-                    break;
-            }
+            aux = borderMargin(x, y, x1, x2, y2, rejaI)
+            x1 = aux.newX1;
+            x2 = aux.newX2;
+            y2 = aux.newY2;
             for (x1; x1 <= x2; x1++) {
-                for (let y1 = y - 1 < 0 ? 0 : y - 1; y1 <= y2; y1++) {
+                for (y1 = y - 1 < 0 ? 0 : y - 1; y1 <= y2; y1++) {
 
                     if ((x1 === rx && ((y1 - 1 === ry) || (y1 + 1 === ry)))
                         || (y1 === ry && ((x1 - 1 === rx) || (x1 + 1 === rx)))) {
