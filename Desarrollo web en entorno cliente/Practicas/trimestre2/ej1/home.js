2b2f077f5399bbcc420cc46545ca294b07b759f8
javiergranizoaguilar <javiercelia123@gmail.com>
2026-01-07 11:40:04 +0100

    new file:   Desarrollo web en entorno cliente/Practicas/trimestre2/ej1/home.css 	new file:   Desarrollo web en entorno cliente/Practicas/trimestre2/ej1/home.html 	new file:   Desarrollo web en entorno cliente/Practicas/trimestre2/ej1/home.js 	new file:   Desarrollo web en entorno cliente/tema3/2TDAW_DWEC_Tema3.pdf 	new file:   Desarrollo web en entorno cliente/tema4/2TDAW_DWEC_Tema4_parte1.pdf

---
+++ Desarrollo web en entorno cliente/Practicas/trimestre2/ej1/home.js
@@ -0,0 +1,31 @@
+let botonC = document.getElementById("botonC");
+let botonV = document.getElementById("botonV");
+let botonL = document.getElementById("botonL"); 
+let lista = document.getElementById("lista");
+
+botonC.addEventListener("click", () => {
+    let clave = prompt("clave");
+    let valor = prompt("valor");
+    localStorage.setItem(clave, valor);
+});
+botonV.addEventListener("click",mostrar);
+lista.addEventListener("dblclick", (e) => {
+    let text = e.target.textContent
+    let clave=text.slice(7,text.lastIndexOf("Valor")).trim();
+    localStorage.removeItem(clave);
+    mostrar();
+});
+botonL.addEventListener("click",()=>{
+    Object.keys(localStorage).forEach(clave=>{
+        localStorage.removeItem(clave);
+    })
+})
+function mostrar() {
+    lista.innerHTML = "";
+
+    Object.keys(localStorage).forEach(clave => {
+        let listaObjeto = document.createElement("li");
+        listaObjeto.textContent = "Clave: " + clave + " Valor: " + localStorage.getItem(clave);
+        lista.appendChild(listaObjeto);
+    });
+}
