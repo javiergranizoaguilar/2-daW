636a70de715dd4346907a2b83c2a2645d8b6d315
javiergranizoaguilar <javiercelia123@gmail.com>
2026-01-16 10:45:30 +0100

    modified:   Desarrollo web en entorno cliente/Entregas/Tema4/formulario.js 	modified:   Desarrollo web en entorno cliente/Entregas/Tema4/home.css

--- Desarrollo web en entorno cliente/Entregas/Tema4/formulario.js
+++ Desarrollo web en entorno cliente/Entregas/Tema4/formulario.js
@@ -1,10 +1,83 @@
 let form = document.getElementById("formulario");
 let numeroColumnas = document.getElementById("numeroColumnas");
 let columnas = document.getElementById("columnas");
-let inputs = document.getElementsByTagName("input");
+let input = document.getElementsByTagName("input");
 let seccionFormulario = document.getElementById("secionFromulario");
 let tablas = document.getElementById("tablas");
-let numero=0;
+
+function localArrray(array, type, clave) {
+    let primero = true;
+    let arrayAux = [];
+    Array.from(array).forEach((e) => {
+        if (e.type === type) {
+            if (primero && type == "number") { primero = !primero }
+            else {
+                arrayAux.push(e.value);
+            }
+        }
+    });
+    localStorage.setItem(clave, JSON.stringify(arrayAux));
+}
+function crearTablas() {
+    tablas.innerHTML = "";
+    let nombresColumnas = JSON.parse(localStorage.getItem("nombres"));
+    for (let x = 0; x < nombresColumnas.length; x++) {
+        let lista = document.createElement("div");
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        p.textContent = nombresColumnas[x];
+        div.appendChild(p);
+        div.appendChild(lista);
+        
+        if (x == 0) {
+            let boton = document.createElement("button");
+            boton.textContent = "crear tarea";
+            boton.id = "crear";
+            div.appendChild(boton);
+        }
+        lista.id = "tabla" + x;
+        tablas.appendChild(div);
+        let listaAux = JSON.parse(localStorage.getItem("lista" + x)) ?? [];
+        listaAux.forEach((e) => {
+            let columnas = document.getElementById("tabla"+x);
+            creaTarea(e,columnas);
+        });
+    }
+    creardragAndDrop();
+
+}
+function creardragAndDrop() {
+    let crearboton = document.getElementById("crear");
+    let tablaOriguinal = document.getElementById("tabla0");
+
+    crearboton.addEventListener("click", () => {
+        let listaTamanio = (JSON.parse(localStorage.getItem("lista0")) ?? 0) == 0 ? 0 : JSON.parse(localStorage.getItem("lista0")).length;
+        let tamanioMaximo = JSON.parse(localStorage.getItem("capacidad"))[0];
+        if (listaTamanio < tamanioMaximo) {
+
+            let tarea = prompt("Escrive la tarea");
+            creaTarea(tarea,tablaOriguinal);
+            recivirDevolverLista(tarea);
+
+        }
+    })
+}
+function creaTarea(tarea,tabla) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.textContent = tarea;
+    div.classList.add("drag");
+    div.appendChild(p);
+    tabla.appendChild(div);
+}
+
+function recivirDevolverLista(tarea) {
+    let nombeLista = "lista0";
+    let listaAux = JSON.parse(localStorage.getItem(nombeLista)) ?? [];
+    listaAux.push(tarea);
+    localStorage.removeItem(nombeLista);
+    localStorage.setItem(nombeLista, JSON.stringify(listaAux));
+}
 
 window.addEventListener("load", () => {
 
@@ -20,18 +93,14 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     localStorage.clear();
+    localArrray(input, "text", "nombres");
+    localArrray(input, "number", "capacidad");
 
-    let columnNames = [];
-    Array.from(inputs).forEach((e) => {
-        if (e.type === "text") {
-            columnNames.push(e.value);
-        }
-    });
-    localStorage.setItem("nombres", JSON.stringify(columnNames));
     seccionFormulario.innerHTML = "";
     crearTablas();
 
 });
+
 
 numeroColumnas.addEventListener("input", () => {
     columnas.innerHTML = "";
@@ -43,55 +112,15 @@ numeroColumnas.addEventListener("input", () => {
         let input = document.createElement("input");
         input.type = "text"
 
+        let inputNumber = document.createElement("input");
+        input.type = "number"
+
         let div = document.createElement("div");
 
         div.appendChild(p);
         div.appendChild(input);
+        div.appendChild(inputNumber);
 
         columnas.appendChild(div);
     }
 });
-function crearTablas() {
-    tablas.innerHTML = "";
-    let nombresColumnas = JSON.parse(localStorage.getItem("nombres"));
-    let primero = true;
-    let n=0;
-    nombresColumnas.forEach((e) => {
-
-        let lista = document.createElement("div");
-        let div = document.createElement("div");
-        let p = document.createElement("p");
-        p.textContent = e;
-        div.appendChild(p);
-        div.appendChild(lista);
-        if (primero) {
-            let boton = document.createElement("button");
-            boton.textContent = "crear tarea";
-            boton.id = "crear";
-            div.appendChild(boton);
-            primero = false
-
-        }
-        lista.id="tabla"+n;
-        tablas.appendChild(div);
-        n++;
-    });
-    creardragAndDrop();
-
-}
-function creardragAndDrop() {
-    let crearboton = document.getElementById("crear");
-    let tablaOriguinal = document.getElementById("tabla0");
-
-    crearboton.addEventListener("click", () => {
-        let tarea =prompt("Escrive la tarea");
-        let div = document.createElement("div");
-        let p = document.createElement("p");
-        p.textContent = tarea;
-        div.id=numero;
-        numero++;
-        div.appendChild(p);
-        tablaOriguinal.appendChild(div);
-        localStorage.setItem("numero",numero);
-    })
-}
